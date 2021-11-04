445aa054f5c8c2d6fae4657011f98bb1ef6b9adb
js-doctrine <92176472+js-doctrine@users.noreply.github.com>
2021-11-04 22:14:19 +0300

    feat: add splash component

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Splash from './components/Splash.vue'
+</script>
+
+<template>
+  <Splash msg="Splash!" />
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/Splash.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+<div class="container">
+        <img width="351" height="415" class="girl" src="../assets/girl.png">
+        <p class="smarter"> Now Gym Smarter with AccuGym</p>
+        <p class="fitness"> An AI based fitness app that can change your life </p> 
+        <button class="get"> Get started </button>
+    </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  background: #20202c;
+  min-height: 100vh;
+  padding: 0px 35px;
+}
+.Height {
+  margin: auto;
+  position: absolute;
+  width: 327px;
+  height: 56px;
+  left: 24px;
+  top: 200px;
+
+  background: #343447;
+  border-radius: 30px;
+}
+.fitness {
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 22px;
+  text-align: center;
+  margin: 0;
+  color: #8a8fa8;
+}
+.get {
+  background-color: transparent;
+  background: linear-gradient(80.52deg, #7959f4 42.3%, #e37af6 119.33%);
+  border-radius: 30px;
+  border: none;
+  outline: none;
+  font-family: Manrope;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 22px;
+  text-align: center;
+  color: #ffffff;
+  padding: 19px 65px;
+  width: fit-content;
+  margin: 57px auto;
+}
+.girl {
+  display: block;
+  margin: 0 auto;
+  }
+</style>
